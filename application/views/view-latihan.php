b2e0effbb1739072923420fdbe957d1556862f8e
rioferdinan99 <skurleferdinan99@gmail.com>
2022-09-26 18:00:54 -0700

    p

---
+++ application/views/view-latihan.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Latihan1</title>
+</head>
+
+<body>
+    Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <? $nilai12; ?>
+    ini hasil dari permodelan dengan metode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " + " . $hasil; ?>
+
+</body>
+
+</html>
